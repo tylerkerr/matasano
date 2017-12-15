2ef38210b9e663acef871b68d1d00c438d054948
tylerkerr <kerr.tyler@gmail.com>
2017-12-15 13:45:36 -0500

    challenge 16

--- set2.py
+++ set2.py
@@ -7,7 +7,9 @@ from cryptography.hazmat.backends import default_backend
 import os
 import sys
 import re
+from time import sleep
 from random import SystemRandom, randint
+from urllib import parse
 
 def padPKCS7(plaintext: bytes, blocksize: int):
     if len(plaintext) % blocksize == 0:
@@ -24,7 +26,7 @@ def padPlaintext(plaintext: bytes, blocksize: int):
         ptblocks[-1] = padPKCS7(ptblocks[-1], blocksize)
     return b''.join(ptblocks)
 
-def stripPKCS7(plaintext: bytes, blocksize: int):
+def deprecated_stripPKCS7(plaintext: bytes, blocksize: int):
     ptblocks = splitToBlocks(plaintext, blocksize)
     if ptblocks[-1] == bytes([blocksize]) * blocksize:
         return b''.join(ptblocks[:-1])
@@ -143,11 +145,11 @@ def decryptCBCOracleSuffix(oracle, blocksize):
         solvedblocks.append(plaintext)
 
 def parseCookie(cookie: str):
-    keys = cookie.split('&')
+    keys = re.split('&|;', cookie)
     dic = {}
     for key in keys:
         k, v = key.split('=')
-        dic[k] = v
+        dic[k] = parse.unquote(v)
     return dic
 
 def makeCookie(email: str, uid: int, role: str):
@@ -167,9 +169,8 @@ def parseProfile(cookie: bytes):
     return cookie
 
 def chal14Encrypt(plaintext: bytes, prebytes: bytes):
-    secret = b64decode('Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK')
+    secret = b64decode('SGF2ZSBhIGJhbmFuYSwgSGFubmFoClRyeSB0aGUgc2FsYW1pLCBUb21teQpHZXQgd2l0aCB0aGUgZ3JhdnksIERhdnkKRXZlcnlib2R5IGVhdHMgd2hlbiB0aGV5IGNvbWUgdG8gbXkgaG91c2UKVHJ5IGEgdG9tYXRvLCBQbGF0bwpIZXJlJ3MgY2FjY2lhdG9yZSwgRG9yeQpUYXN0ZSBvZiBib2xvZ25hLCBUb255CkV2ZXJ5Ym9keSBlYXRzIHdoZW4gdGhleSBjb21lIHRvIG15IGhvdXNlCg==')
     key = b64decode('gR/1a5kNvr2pTK3BOX/m7Q==') # random but permanent
-    
     oraclept = padPKCS7(prebytes + plaintext + secret, 16)
     return encryptAESECB(oraclept, key)
 
@@ -182,6 +183,127 @@ def detectOracleBlocksizeHard(oracle, prebytes: bytes):
         newlength = len(oracle((lengthtest.encode()), prebytes))
     return newlength - baselength
 
-def decryptCBCOracleSuffixHard(oracle, prebytes: bytes, blocksize: int):
-    return True
+def detectOraclePrebytes(oracle, prebytes: bytes, blocksize: int):
+    null = oracle(b'', prebytes)
+    atest = oracle(b'a', prebytes)
+    preblocks = 0
+    blockcheck = 0
+    while True:
+        if splitToBlocks(null, blocksize)[blockcheck] == splitToBlocks(atest, blocksize)[blockcheck]:
+            preblocks += 1
+            blockcheck += 1
+        else:
+            break
+    lengthtest = 'a'
+    while True:
+        currentcheck = oracle(lengthtest.encode(), prebytes)
+        lengthtest += 'a'
+        nextcheck = oracle(lengthtest.encode(), prebytes)
+        foundblocks = 0
+        blockcheck = 0
+        while True:
+            if splitToBlocks(currentcheck, blocksize)[blockcheck] == splitToBlocks(nextcheck, blocksize)[blockcheck]:
+                foundblocks += 1
+                blockcheck += 1
+            else:
+                break
+        if foundblocks > preblocks:
+            detectedbytes = preblocks * blocksize + (blocksize - len(lengthtest) + 1)
+            return detectedbytes
+
+def decryptCBCOracleSuffixHard(oracle, prebytes: bytes, blocksize: int, 
+                               prefixlen: int, prefixpad: bytes):
+    prefixblocks = (prefixlen + len(prefixpad)) // blocksize
+    prefixbytes = prefixblocks * blocksize
+    print("[+] discarding {} blocks of (padded) prefix".format(prefixblocks))
+
+    totalblocks = (len(oracle(prefixpad + b'', prebytes)[prefixbytes:]) // blocksize)
+    print("[+] secret is {} blocks long".format(totalblocks))
+
+    allbytes = [bytes([i]) for i in range(256)]
+    
+    filler = ('X' * blocksize).encode()
+    solvedblocks = []
+    for block in range(totalblocks):
+        plaintext = b''
+        slicestart = prefixbytes+(block*blocksize)
+        sliceend = prefixbytes+(block*blocksize)+blocksize
+        if block == 0:
+            testblock = filler
+        else:
+            testblock = solvedblocks[block-1]
+        for bytepos in range(blocksize):
+            test = testblock[bytepos+1:]
+            pretarget = oracle(prefixpad + test, prebytes)
+            target = pretarget[slicestart:sliceend]
+            for byte in allbytes:
+                checkblock = test + plaintext + byte
+                if oracle(prefixpad + checkblock, prebytes)[prefixbytes:prefixbytes+blocksize] == target:
+                    plaintext += byte
+                    print(byte.decode(), end='')
+                    sys.stdout.flush()
+                    break
+        solvedblocks.append(plaintext)
+
+def stripPKCS7(plaintext: bytes, blocksize: int):
+    lastblock = plaintext[-blocksize:]
+    if lastblock == bytes([blocksize] * blocksize): # if it's all padding bytes, it aligned with the blocksize
+        return plaintext[:blocksize] # trim the entire block
+    padbyte = lastblock[-1]
+    padding = lastblock[-padbyte:]
+    if not padbyte in [x for x in range(blocksize)]: # if the final byte isn't one of the possible pad bytes
+        raise ValueError('[!] bad PCKS#7 padding')
+    elif padding != bytes([padbyte]) * padbyte: # if the padding isn't all the same byte
+        raise ValueError('[!] bad PCKS#7 padding')
+    else:
+        return(plaintext[:-padbyte]) # strip it
+
+def chal16Cookie(userdata: str):
+    key = b64decode('M3lWZiQ04Zqfy+KG9mHgvg==')
+    iv = os.urandom(16)
+    prepend = 'comment1=cooking%20MCs;userdata='
+    append = ';comment2=%20like%20a%20pound%20of%20bacon'
+    userdata = re.sub(';', '%3B', userdata)
+    userdata = re.sub('=', '%3D', userdata)
+    cookie = prepend + userdata + append
+    print(cookie)
+    return (encryptAESCBC(padPKCS7(cookie.encode(), 16), key, iv), iv)
+
+
+def isAdmin(dic: dict):
+    try:
+        assert dic['admin']
+        return True
+    except:
+        return False
+
+def chal16Parse(cookie: tuple):
+    key = b64decode('M3lWZiQ04Zqfy+KG9mHgvg==')
+    iv = cookie[1]
+    plaintext = stripPKCS7(decryptAESCBC(cookie[0], key, iv), 16).decode()
+    return parseCookie(plaintext)
+
+def char2bin(char: str):
+    return bin(ord(char))[2:].zfill(8)
+
+def bin2char(binary: str):
+    return chr(int(binary, 2))
+
+def allBitFlips(char: str):
+    binary = char2bin(char)
+    print(binary, char)
+    for b in enumerate(binary):
+        pos = b[0]
+        bit = b[1]
+        flipbin = binary[:pos] + str(int(not int(bit))) + binary[pos+1:]
+        print(flipbin, chr(int(flipbin, 2)))
+
+def flipBit(byte: bytes, flip: int):
+    assert len(byte) == 1
+    assert flip <= 7
+    binary = char2bin(chr(ord(byte)))
+    flipped = binary[:flip] + str(int(not int(binary[flip]))) + binary[flip+1:]
+    return bytes([int(flipped, 2)])
+
+
 
--- set2chals.py
+++ set2chals.py
@@ -76,8 +76,8 @@ def chal13():
     the cookie looks like this:
     email=fake@evil.net&uid=93&role=admin&uid=62&role=user
     '''
-    # evilcookie = splitToBlocks(makeProfile('fake@evil.admin'), 16)
-    # nicecookie = splitToBlocks(makeProfile('fakefakefakefakefake@fake.net'), 16)
+    # evilcookie = splitToBlocks(makeProfile('me@tyler.admin'), 16)
+    # nicecookie = splitToBlocks(makeProfile('notarealemailaddress@web.bike'), 16)
     # admincookie = evilcookie[0] + nicecookie[2] + evilcookie[1] + evilcookie[2]
     # print(parseCookie(parseProfile(admincookie)))
 
@@ -99,6 +99,7 @@ def chal13():
     but we have to be able to submit this as an email:
     fake@evil.admin\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b
     '''
+    print('[-] using an email containing PKCS#7 padding')
     evilcookie = splitToBlocks(makeProfile('fake@evil.' + padPKCS7(b'admin', 16).decode()), 16)
     nicecookie = splitToBlocks(makeProfile('me@tyler.bike'), 16)
     admincookie = nicecookie[0] + nicecookie[1] + evilcookie[1]
@@ -111,6 +112,7 @@ def chal13():
     the cookie looks like this:
     email=me@tyler.bike&uid=67&role=admin&uid=64&rolole=user
     '''
+    print('[-] using invalid parameters')
     evilcookie = splitToBlocks(makeProfile('fake@evil.admin'), 16)
     fillcookie = splitToBlocks(makeProfile('fakefake@garb.net'), 16)
     nicecookie = splitToBlocks(makeProfile('me@tyler.bike'), 16)
@@ -122,15 +124,78 @@ def chal13():
 
 
 def chal14():
-    prebytes = os.urandom(SystemRandom().randint(1, 64))
+    print('[-] trying challenge fourteen')
+    prebytes = os.urandom(SystemRandom().randint(16, 16))
     blocksize = detectOracleBlocksizeHard(chal14Encrypt, prebytes)
     print("[+] blocksize is", blocksize)
     assert detectECB(chal14Encrypt(open('./samples/books/candide.txt', 'rb').read(), prebytes), blocksize)
     print("[+] successfully detected ECB")
+    prefixlen = detectOraclePrebytes(chal14Encrypt, prebytes, blocksize)
+    if prefixlen % blocksize != 0:
+        prefixpad = b'a' * (blocksize - (prefixlen % blocksize))
+    else:
+        prefixpad = b''
+    print("[+] detected {} prefix bytes, using a pad of length {}".format(prefixlen, len(prefixpad)))
+    decryptCBCOracleSuffixHard(chal14Encrypt, prebytes, blocksize, prefixlen, prefixpad)
+    print('[+] challenge fourteen successful')
+
+
+def chal15():
+    print('[-] trying challenge fifteen')
+    goodtest = b"ICE ICE BABY\x04\x04\x04\x04"
+    longtest = b"YELLOW SUBMARINE\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10"
+    bothtest = b"YELLOW SUBMARINEICE ICE BABY\x04\x04\x04\x04"
+    badtest1 = b"ICE ICE BABY\x05\x05\x05\x05"
+    badtest2 = b"ICE ICE BABY1234"
+    badtest3 = b"YELLOW SUBMARINEICE ICE BABY\x05\x05\x05\x05"
+    assert stripPKCS7(goodtest, 16) == b'ICE ICE BABY'
+    assert stripPKCS7(longtest, 16) == b'YELLOW SUBMARINE'
+    assert stripPKCS7(bothtest, 16) == b'YELLOW SUBMARINEICE ICE BABY'
+    try:
+        print(stripPKCS7(badtest1, 16))
+        print('[!] failed to raise exception on bad padding!')
+    except ValueError:
+        pass
+    try:
+        print(stripPKCS7(badtest2, 16))
+        print('[!] failed to raise exception on bad padding!')
+    except ValueError:
+        pass
+    try:
+        print(stripPKCS7(badtest3, 16))
+        print('[!] failed to raise exception on bad padding!')
+    except ValueError:
+        pass
+    print('[+] all tests passed')
+    print('[+] challenge fifteen successful')
+
+def chal16():
+    '''
+    comment1=cooking %20MCs;userdata= flip9admin5true; comment2=%20like %20a%20pound%20o f%20bacon
+    0000000000000000 1111111111111111 2222222222222222 3333333333333333 4444444444444444 5555555555555555
+    '''
+    print('[-] trying challenge sixteen')
+    cookie = chal16Cookie('hey ;admin=true;dudes')
+    print(chal16Parse(cookie))
+    print(isAdmin(chal16Parse(cookie)))
+    print("[-] all bitflips for '=':")
+    allBitFlips('=')
+    print("[+] using 9 (flip bit 5)")
+    print("[-] all bitflips for ';':")
+    allBitFlips(';')
+    print("[+] using 3 (flip bit 4)")
+    flipBit(b'=', 5)
+
 
 # chal9()
 # chal10()
 # chal11()
 # chal12()
 # chal13()
-chal14()
+# chal14()
+# chal15()
+chal16()
+
+
+
+
