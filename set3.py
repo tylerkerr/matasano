4aa594d5eb7b308ba63c86ab33e9ba906b851d16
tylerkerr <kerr.tyler@gmail.com>
2017-12-20 12:50:49 -0500

    challenge 19

--- set3.py
+++ set3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-from set1 import splitToBlocks, fixedXOR
-from set2 import padPKCS7, stripPKCS7, encryptAESCBC, decryptAESCBC
+from set1 import splitToBlocks, fixedXOR, singleXOR
+from set2 import padPKCS7, stripPKCS7, encryptAESCBC, decryptAESCBC, encryptAESECB
 import os
 import sys
 import re
@@ -68,3 +68,61 @@ def attackCBCPaddingOracle(ciphertext: tuple, oracle):
         pt.append(ptblock)
 
     return stripPKCS7(b''.join(pt), 16).decode()
+
+
+def AESCTR(inp: bytes, key: bytes, nonce: bytes):
+    inblocks = splitToBlocks(inp, 16)
+    outblocks = []
+    counter = 0
+    for block in inblocks:
+        ctrbytes = counter.to_bytes(8, byteorder='little')
+        prestream = nonce + ctrbytes
+        keystream = encryptAESECB(prestream, key)
+        if len(block) != len(keystream):
+            keystream = keystream[:len(block)]
+        outblocks.append(fixedXOR(block, keystream))
+        counter += 1
+    return b''.join(outblocks)
+
+
+def chal19Encrypt():
+    pts = ['SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==', 'Q29taW5nIHdpdGggdml2aWQgZmFjZXM=', 'RnJvbSBjb3VudGVyIG9yIGRlc2sgYW1vbmcgZ3JleQ==', 'RWlnaHRlZW50aC1jZW50dXJ5IGhvdXNlcy4=', 'SSBoYXZlIHBhc3NlZCB3aXRoIGEgbm9kIG9mIHRoZSBoZWFk', 'T3IgcG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==', 'T3IgaGF2ZSBsaW5nZXJlZCBhd2hpbGUgYW5kIHNhaWQ=', 'UG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==', 'QW5kIHRob3VnaHQgYmVmb3JlIEkgaGFkIGRvbmU=', 'T2YgYSBtb2NraW5nIHRhbGUgb3IgYSBnaWJl', 'VG8gcGxlYXNlIGEgY29tcGFuaW9u', 'QXJvdW5kIHRoZSBmaXJlIGF0IHRoZSBjbHViLA==', 'QmVpbmcgY2VydGFpbiB0aGF0IHRoZXkgYW5kIEk=', 'QnV0IGxpdmVkIHdoZXJlIG1vdGxleSBpcyB3b3JuOg==', 'QWxsIGNoYW5nZWQsIGNoYW5nZWQgdXR0ZXJseTo=', 'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=', 'VGhhdCB3b21hbidzIGRheXMgd2VyZSBzcGVudA==', 'SW4gaWdub3JhbnQgZ29vZCB3aWxsLA==', 'SGVyIG5pZ2h0cyBpbiBhcmd1bWVudA==', 'VW50aWwgaGVyIHZvaWNlIGdyZXcgc2hyaWxsLg==', 'V2hhdCB2b2ljZSBtb3JlIHN3ZWV0IHRoYW4gaGVycw==', 'V2hlbiB5b3VuZyBhbmQgYmVhdXRpZnVsLA==', 'U2hlIHJvZGUgdG8gaGFycmllcnM/', 'VGhpcyBtYW4gaGFkIGtlcHQgYSBzY2hvb2w=', 'QW5kIHJvZGUgb3VyIHdpbmdlZCBob3JzZS4=', 'VGhpcyBvdGhlciBoaXMgaGVscGVyIGFuZCBmcmllbmQ=', 'V2FzIGNvbWluZyBpbnRvIGhpcyBmb3JjZTs=', 'SGUgbWlnaHQgaGF2ZSB3b24gZmFtZSBpbiB0aGUgZW5kLA==', 'U28gc2Vuc2l0aXZlIGhpcyBuYXR1cmUgc2VlbWVkLA==', 'U28gZGFyaW5nIGFuZCBzd2VldCBoaXMgdGhvdWdodC4=', 'VGhpcyBvdGhlciBtYW4gSSBoYWQgZHJlYW1lZA==', 'QSBkcnVua2VuLCB2YWluLWdsb3Jpb3VzIGxvdXQu', 'SGUgaGFkIGRvbmUgbW9zdCBiaXR0ZXIgd3Jvbmc=', 'VG8gc29tZSB3aG8gYXJlIG5lYXIgbXkgaGVhcnQs', 'WWV0IEkgbnVtYmVyIGhpbSBpbiB0aGUgc29uZzs=', 'SGUsIHRvbywgaGFzIHJlc2lnbmVkIGhpcyBwYXJ0', 'SW4gdGhlIGNhc3VhbCBjb21lZHk7', 'SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=', 'VHJhbnNmb3JtZWQgdXR0ZXJseTo=', 'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=']
+    key = b'uYTesOYLHdvqveJmGIcBgQ=='
+    nonce = bytes(8)
+    cts = [AESCTR(b64decode(pt), key, nonce) for pt in pts]
+    return cts
+
+def getMaxLen(l: list):
+    maxlen = 0
+    for i in l:
+        if len(i) > maxlen:
+            maxlen = len(i)
+    return maxlen
+
+def getMinLen(l: list):
+    minlen = getMaxLen(l)
+    for i in l:
+        if len(i) < minlen:
+            minlen = len(i)
+    return minlen
+
+
+def bruteforceSingleXORBytes(ciphertext: str, charscores: dict):  # hex
+    trialdecrypts = {}
+    for byte in range(256):
+        trialdecrypts[byte] = bytearray(singleXOR(ciphertext, byte))
+    keyscores = {}
+    for trial in trialdecrypts:
+        score = 0
+        for c in trialdecrypts[trial]:
+            char = chr(c)
+            if char in charscores:
+                score += charscores[char]
+            else:
+                score -= 1
+        keyscores[trial] = score
+        sortedscores = [(c, keyscores[c]) for c in sorted(keyscores, key=keyscores.get, reverse=True)]
+    bestkey = sortedscores[0][0]
+    score = sortedscores[0][1]
+    plaintext = trialdecrypts[bestkey]
+    return bestkey, plaintext
--- set3chals.py
+++ set3chals.py
@@ -1,15 +1,58 @@
 #!/usr/bin/env python3
 
 from set3 import *
+from set1 import buildCorpus
 
 def chal17():
     print('[-] trying challenge seventeen')
     assert chal17Oracle(chal17Encrypt())
-    fakect = (b'block of sixteenblock of sixteenblock of sixteenpadthis', b'yellow submarine')
+    fakect = (b'block of sixteenblock of sixteenpadthis', b'yellow submarine')
     assert not chal17Oracle(fakect)
     plaintext = attackCBCPaddingOracle(chal17Encrypt(), chal17Oracle)
     assert re.match('^\d{6}', plaintext)
     print(plaintext)
     print('[+] challenge seventeen successful')
 
+def chal18():
+    print('[-] trying challenge eighteen')
+    ciphertext = b64decode('L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==')
+    nonce = bytes(8)
+    key = b'YELLOW SUBMARINE'
+    plaintext = AESCTR(ciphertext, key, nonce).decode()
+    print(plaintext)
+
+    nonce = os.urandom(8)
+    testpt = b'encrypt this immediately, i command it'
+    testct = AESCTR(testpt, key, nonce)
+    assert testpt == AESCTR(testct, key, nonce)
+    print('[+] challenge eighteen successful')
+
+def chal19():
+    print('[-] trying challenge nineteen')
+    charscores = buildCorpus('./samples/books/')
+    cts = chal19Encrypt()
+    maxlen = getMaxLen(cts)
+    minlen = getMinLen(cts)
+    cracked = []
+    for byte in range(minlen):
+        array = []
+        for ct in cts:
+            array.append(ct[byte-1])
+        bestbyte, plaintext = bruteforceSingleXORBytes(bytes(array), charscores)
+        cracked.append(plaintext)
+    plaintexts = []
+    for pos in range(len(cts)):
+        pt = bytes([c[pos] for c in cracked])
+        plaintexts.append(pt)
+    knownpt = open('./data/easter.txt', 'r').read().splitlines()
+    for line in range(len(knownpt)):
+        if len(knownpt[line]) == maxlen:
+            keystream = fixedXOR(knownpt[line].encode(), cts[line])
+    for ct in cts:
+        print(fixedXOR(ct, keystream[:len(ct)]).decode())
+    print('[+] challenge nineteen successful')
+
+
 chal17()
+chal18()
+chal19()
